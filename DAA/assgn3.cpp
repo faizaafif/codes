b376ee05f557ce6e1c481efda8b4431663aa60d8
faiza <149992729+faizaafif@users.noreply.github.com>
2025-02-04 20:49:26 +0530

    assgn3.cpp

--- DAA/assgn3.cpp
+++ DAA/assgn3.cpp
@@ -6,7 +6,7 @@ class graph{
 	int cities;
 	public:
 		graph(){
-			cout<<"Enter number of cities :"<<endl;
+			cout<<"Enter number of cities : ";
 			cin>>cities;
 			for(int i=0; i<cities; i++){
             			for(int j=0; j<cities; j++){
@@ -19,11 +19,11 @@ class graph{
 			char ch;
 			for(int i=0; i<cities; i++){
 				for(int j=0; j<cities; j++){
-					if(i != j && cost[i][j] == 99){
-						cout<<"Is there a connection between"<< i<< "&"<< j<<"?"<<endl;
+					if(i != j && cost[i][j] == 999){
+						cout<<"Is there a connection between "<< i<< " & "<< j<<"? ";
 						cin>>ch;
 						if(ch =='y' || ch == 'Y'){
-							cout<<"Enter cost of edge\n";
+							cout<<"Enter cost of edge : ";
 							cin>>cost[i][j];
 							cost[j][i]=cost[i][j];
 						}
@@ -35,7 +35,7 @@ class graph{
 		void display(){
 			for(int i=0; i<cities; i++){
 				for(int j=0; j<cities; j++){
-					cout<<cost[i][j]<<endl;
+					cout<<cost[i][j]<<"\t";
 				}	
 				cout<<endl;
 			}
@@ -55,9 +55,9 @@ class graph{
 					nearest[i]= start_v;
 				}
 			}
-			for(int i=1; i< n-1; i++){
+			for(int i=0; i< n-1; i++){
 				min = 999;
-				for(int k=0; k<n-1; k++){
+				for(int k=0; k<n; k++){
 					if(nearest[k]= -1 && (cost[k][nearest[k]] < min)){
 						index = k;
 						min = cost[k][nearest[k]] ;
@@ -69,7 +69,7 @@ class graph{
 				r++;
 				mincost +=cost[index][nearest[index]];
 				nearest[index] =-1;
-				for(int k=0; k<n-1; k++){
+				for(int k=0; k<n; k++){
 					if(nearest[k]= -1 && (cost[k][nearest[k]] > cost[k][index])){
 						nearest[k] = index;
 					}
@@ -83,8 +83,8 @@ int main(){
 	graph city;
 	int  ch;
 	do{
-		cout<<"Enter user's choice :"<<endl;
-		cout<<"1.Create\n2.Display\n3.Prims\n4.Exit";
+		cout<<"1.Create\n2.Display\n3.Prims\n4.Exit\n";
+		cout<<"Enter user's choice : ";
 		cin>>ch;
 		switch(ch){
 			case 1:
@@ -96,7 +96,7 @@ int main(){
 			break;
 			
 			case 3:
-			city.prims(0);
+			cout<<city.prims(0)<<endl;
 			break;
 			
 			case 4:
